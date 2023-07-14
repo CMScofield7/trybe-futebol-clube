3b652ba55fbe74f474644678c0a78e98521c6c2f
[Scofield] Laion Brito <scoundreldays2@gmail.com>
2023-07-14 03:43:43 -0300

    Requisito 1 Concluído! - Desenvolvimento da migration e model de Teams

---
+++ app/backend/src/database/migrations/01-create-teams-table.ts
@@ -0,0 +1,26 @@
+import { Model, QueryInterface, DataTypes } from "sequelize";
+import ITeams from "../../Interfaces/ITeams";
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<ITeams>>('teams', {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      teamName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'team_name',
+      }
+    });
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('teams');
+  },
+
+};
---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,36 @@
+import { 
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+ } from 'sequelize';
+
+ import db from '.';
+
+ class Teams extends Model<InferAttributes<Teams>, InferCreationAttributes<Teams>> {
+  declare id: CreationOptional<number>;
+  declare teamName: CreationOptional<string>;
+ }
+
+ Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    // references: {
+    //   model: 'matches',
+    //   key: 'id',
+    // }
+  }
+ }, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+ });
